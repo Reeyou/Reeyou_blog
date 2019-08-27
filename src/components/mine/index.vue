f4d81da9aff761c798b4aa803c29239577d618b7
Reeyou <ryu993830137@163.com>
2019-08-27 22:08:57 +0800

    添加博客首页及组件

---
+++ src/components/mine/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class='mine'>
+    <div class="avatar">
+      <img src="../../assets/reeyou.jpg" alt="">
+    </div>
+    <div class="nickName">Reeyou</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+  .mine {
+    width: 100%;
+    text-align: center;
+    .avatar img{
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+    }
+    .nickName {
+      color: #000;
+      font-size: 14px;
+      margin: 20px 0;
+    }
+  }
+</style>
