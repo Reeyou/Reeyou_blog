f3ab22655c184e9dc314a5db6524bd7179033839
Reeyou <ryu993830137@163.com>
2019-10-06 19:14:04 +0800

    更新README

--- test11/config/dev.env.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  // BASE_API: '"http://106.54.44.253:3000"',
-  BASE_API: '"http://localhost:3000"',
-})
